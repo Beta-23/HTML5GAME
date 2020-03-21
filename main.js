adae964b18959eef5f5400ff4ab8a92961593a66
Beta-23 <alfred.dominguez@gmail.com>
2020-03-20 20:29:08 -0700

    Create gameboard functionality

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
+// create canvas element via js
 var canvas = document.createElement("canvas");
 var context = canvas.getContext("2d");
 canvas.height = 600;
 canvas.width = 800;
+// new image object
+mainImage = new Image();
+mainImage.ready = false;
+mainImage.onload = checkReady;
+mainImage.src = "pac.png";
+
+function checkReady() {
+    this.ready = true;
+    playgame();
+}
+
+function playgame() {
+    render();
+}
+// Content
+function render() {
+    context.fillStyle = "black";
+    context.fillRect(0,0,canvas.width,canvas.height);
+}
+
 document.body.appendChild(canvas);
 context.fillText("Lets Play PAC!",10,150);
