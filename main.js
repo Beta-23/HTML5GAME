d4a533de5c1f01d6d383e4371040fa87233c63d3
Beta-23 <alfred.dominguez@gmail.com>
2020-03-20 19:58:50 -0700

    Refactor javascript to create canvas object

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-var canvas = document.getElementById('canvasSpace');
+var canvas = document.createElement("canvas");
 var context = canvas.getContext("2d");
+canvas.height = 600;
+canvas.width = 800;
+document.body.appendChild(canvas);
 context.fillText("Lets Play PAC!",10,150);
